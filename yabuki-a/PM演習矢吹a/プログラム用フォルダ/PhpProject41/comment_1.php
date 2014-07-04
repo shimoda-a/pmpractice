c37aaaaecd61ba161714f9e5ef9432fbd19fd2d6
satoshimoriya <s1242116nc@s.chibakoudai.jp>
2014-07-04 13:25:18 +0900

    コメント欄完成

--- yabuki-a/PM演習矢吹a/プログラム用フォルダ/PhpProject41/comment_1.php
+++ yabuki-a/PM演習矢吹a/プログラム用フォルダ/PhpProject41/comment_1.php
@@ -1,12 +1,9 @@
 <?php
-$comment = $_POST["comment"]; //formのname属性で使ったものを使う
+header("HTTP/1.1 301 Moved Permanetly");
+header("Location: commentform_1.php");
+$comment = $_POST["comment"]; 
 $user = $_POST["user"];
 
-//送信されたデータを表示
-//echo $comment;
-//echo $user;
-//送信されたデータをデータベースに保存
-//データベースに接続（レシピ260）
 try {
     $dsn = 'mysql:host=localhost;dbname=ramens;charset=utf8';
     $dbUser = 'test';
@@ -17,7 +14,6 @@ try {
     echo "接続できませんでした" . $e->getMessage();
 }
 
-//データの挿入（レシピ265）
 try {
     $sql = "INSERT INTO comments (comment, user) VALUES (:comment, :user)";
     $prepare = $db->prepare($sql);
@@ -26,4 +22,5 @@ try {
     $prepare->execute();
 } catch (PDOException $e) {
     echo "挿入失敗" . $e->getMessage();
+    
 }
--- yabuki-a/PM演習矢吹a/プログラム用フォルダ/PhpProject41/view_1.php
+++ yabuki-a/PM演習矢吹a/プログラム用フォルダ/PhpProject41/view_1.php
@@ -12,7 +12,6 @@ and open the template in the editor.
     <body>
 <?php
 
-//データベースに接続（レシピ260）
 try {
 $dsn = 'mysql:host=localhost;dbname=ramens;charset=utf8';
 $dbUser = 'test';
@@ -23,20 +22,15 @@ $db = new PDO($dsn, $dbUser, $dbPass);
     echo "接続できませんでした".$e->getMessage();
 }
 
-//データの表示（レシピ261）
 try{
 $sql = "SELECT comment, user from comments";
 $prepare = $db->prepare($sql);
 $prepare->execute();
 $result = $prepare->fetchAll(PDO::FETCH_ASSOC);
 
-//簡易表示
-//print_r($result);
-
-//配列の処理（レシピp.92）
 echo '<dl>';
 foreach ($result as $comment) {
-    //print_r($comment);
+
     $body = $comment['comment'];
     $user = $comment['user'];
     echo "<dt>$user</dt><dd>$body</dd>"; 
