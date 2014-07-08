b93b3dcb9a91db0479d92d6a22a3c43102c149c9
JunWakatsuki <s1242132HB@s.chibakoudai.jp>
2014-07-08 20:09:41 +0900

    プロジェクト作成

---
+++ yabuki-a/PhpProject3/view.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+<?php
+
+try {
+$dsn = 'mysql:host=localhost;dbname=ramens;charset=utf8';
+$dbUser = 'test';
+$dbPass = 'pass';
+        
+$db = new PDO($dsn, $dbUser, $dbPass);
+} catch (PDOException $e) {
+    echo "接続できませんでした".$e->getMessage();
+}
+
+try{
+$sql = "SELECT name, comments from comments";
+$prepare = $db->prepare($sql);
+$prepare->execute();
+$result = $prepare->fetchAll(PDO::FETCH_ASSOC);
+
+echo '<dl>';
+foreach ($result as $comments) {
+    
+    $name = $comments['name'];
+    $comments = $comments['comments'];  
+    echo "<dt>$name</dt><dt>$comments</dt>"; 
+}
+echo '</dl>';
+
+} catch (PDOException $e) {
+    echo "取得失敗".$e->getMessage();
+}
+
+?>
+</body>
+</html>
