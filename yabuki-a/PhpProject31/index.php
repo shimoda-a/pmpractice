ebe296ffc83fdf5b61c3aa9e1931aec1ae239473
JunWakatsuki <s1242132HB@s.chibakoudai.jp>
2014-07-16 16:28:41 +0900

    php編集

---
+++ yabuki-a/PhpProject31/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html "-//W3C//DTD XHTML 1.0 Strict//EN" 
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+  <title>Google Maps JavaScript API サンプル</title>
+  <div id="map" style="width: 740px; height: 400px;"></div>
+  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+  <script type="text/javascript" src="home.js"></script></head>
+<body></body>
+</html>
