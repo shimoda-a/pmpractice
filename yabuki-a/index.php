1318433cadb7dd9f97b35a25d36319e6ca87aa91
JunWakatsuki <s1242132HB@s.chibakoudai.jp>
2014-07-17 18:39:50 +0900

    矢吹bが作ってくれたファイルにしました

--- yabuki-a/index.php
+++ yabuki-a/index.php
@@ -1,15 +1,2 @@
-<<<<<<< HEAD
-<!DOCTYPE html "-//W3C//DTD XHTML 1.0 Strict//EN" 
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-  <title>ラーメンマップ</title>
-  <div id="map" style="width: 740px; height: 400px;"></div>
-  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
-  <script type="text/javascript" src="index.js"></script></head>
-<body></body>
-</html>
-=======
-<?php echo 'hello';
->>>>>>> origin/master
+<?php
+header ('Location:logingamen.php');
