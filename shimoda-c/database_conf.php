498dbf86fa5ef9196a07bda6bd62dc0dee7608a6
turutyaaaaaaaaaaaann <g.u-star_nrs@i.softbank.jp>
2014-07-17 22:35:54 +0900

    Revert "turutyaaaaaaaaaaaann"
    This reverts commit be1b4fd542b32cb4083e20e47f69d4d10227a44f.

--- shimoda-c/database_conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-// データベース設定
-$dbServer = '192.168.0.1';
-$dbUser = 'cxlw8gn_awq';
-$dbPass = 'C8zjdMl3';
-$dbName = 'kakeibo_system';
-
-# MySQL用のDSN文字列です。
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
