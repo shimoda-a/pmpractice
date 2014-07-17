93569f462fb1c0246e4e83d06bcaac5df1f2a2eb
turutyaaaaaaaaaaaann <g.u-star_nrs@i.softbank.jp>
2014-07-18 00:53:12 +0900

    shimoda-c
    sync

---
+++ shimoda-c/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body bgcolor="#ffdead" text="#000000" link="#0000ff" vlink="#800080" alink="#ff0000">
+        <div align="center"> <font size="10"  >
+            <br><br>ようこそ矢吹様
+            <br><br>MENU
+            <br><br><p><input type="button"style="WIDTH: 400px; HEIGHT: 40px"font size="10"onClick="location.href='tweetform3_1.php'" value="入力画面" ></p>
+            <p><input type="button"style="WIDTH: 400px; HEIGHT: 40px"onClick="location.href='search_2_1_1_1.php'" value="収支画面" ></a></p></div>
+    
+    </body>
+ 
