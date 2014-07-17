12f348d8f91dec87c33025fcec4c61020cdedb1e
JunWakatsuki <s1242132HB@s.chibakoudai.jp>
2014-07-17 17:59:21 +0900

    Update index.php

--- yabuki-a/index.php
+++ yabuki-a/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html "-//W3C//DTD XHTML 1.0 Strict//EN" 
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -10,6 +9,3 @@
   <script type="text/javascript" src="index.js"></script></head>
 <body></body>
 </html>
-=======
-<?php echo 'hello';
->>>>>>> origin/master
