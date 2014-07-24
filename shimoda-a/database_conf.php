67f4d75c9f15f8583c7ea9bcaa46fc1597898de7
shoobata <s1142033ea@s.chibakoudai.jp>
2014-07-25 08:31:46 +0900

    test
    できていますか？

---
+++ shimoda-a/database_conf.php
@@ -0,0 +1,9 @@
+<?php
+// データベース設定
+$dbServer = '175.184.33.65';
+$dbUser = 'cl7db3z_ae6';
+$dbPass = 'pYRBGi9y';
+$dbName = 'cl7db3z_ae6';
+
+# MySQL用のDSN文字列です。
+$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
