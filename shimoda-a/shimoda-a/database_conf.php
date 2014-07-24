c31794bd5a267f8e3085ec9c59d9f9a1f120cb4d
shoobata <s1142033ea@s.chibakoudai.jp>
2014-07-25 08:30:34 +0900

    Revert "コードレビュー"
    This reverts commit 310c614d93cb9d1d866b3b5de4ed528b980edc18.

--- shimoda-a/shimoda-a/database_conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-// データベース設定
-$dbServer = '175.184.33.65';
-$dbUser = 'cl7db3z_ae6';
-$dbPass = 'pYRBGi9y';
-$dbName = 'cl7db3z_ae6';
-
-# MySQL用のDSN文字列です。
-$dsn = "mysql:host={$dbServer};dbname={$dbName};charset=utf8";
